02e7fbd24b87bab791b0b98ffa4f98473038e525
Alexandra Olsavsky <alexandraolsavsky@gmail.com>
2022-08-19 17:54:23 -0500

    basic component folder structure added

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+    const header = 'I am the header!';
+
+    return (
+        <div className="container">
+            <h2>{header}</h2>
+        </div>
+    );
+}
+
+export default Header;
