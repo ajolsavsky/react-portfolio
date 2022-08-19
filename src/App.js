e85e59a96e1f593cd4fe3f14a51611c9eba001e4
Alexandra Olsavsky <alexandraolsavsky@gmail.com>
2022-08-19 18:21:22 -0500

    added basic navbar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import CssBaseline from '@mui/material/CssBaseline';
+
 
 function App() {
   return (
     <div className="App">
+      <CssBaseline />
       <Header />
-      Hello world!
+      Hello all.
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function Header() {
-    const header = 'I am the header!';
-
     return (
-        <div className="container">
-            <h2>{header}</h2>
-        </div>
-    );
-}
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                Alex Olsavsky
+              </Typography>
+              <IconButton
+                size="large"
+                edge="end"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 0 }}
+              >
+                <MenuIcon />
+              </IconButton>
+            </Toolbar>
+          </AppBar>
+        </Box>
+      );
+    }
+
+// function Header() {
+//     const header = 'I am the header!';
+
+//     return (
+//         <div className="container">
+//             <h2>{header}</h2>
+//         </div>
+//     );
+// }
 
 export default Header;
