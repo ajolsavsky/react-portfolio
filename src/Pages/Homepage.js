a5b189ecbb89993ced7d3a4321541f72d9187a6f
Alexandra Olsavsky <alexandraolsavsky@gmail.com>
2022-08-25 00:44:27 -0500

    homepage styling updated

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -2,23 +2,46 @@ import React from "react";
 import About from "../components/About";
 import {
     Container,
-    Box
+    Box,
+    Grid,
+    Item,
+    Typography
 } from '@mui/material';
 import headshot from "../images/headshot.png"
+import Image from 'react-bootstrap/Image'
 
 function homePage() {
     return (
         <>
         <Box>
+        <Grid container direction="row" justifyContent="center" alignItems="center">
+  <Grid item xs>
+    <Box sx={{m: 10}}>
+        <Typography variant="h2" sx={{ color: "primary.dark" }}>Hi, I'm Alex Olsavsky!</Typography>
+        <Typography variant="h5">I'm a software developer with roots as a nonprofit leader and professional singer.</Typography>
+    </Box>
+  </Grid>
+  <Grid item xs>
+  <Box sx={{mt: 10, mr: 5 }}>
+  <Image roundedCircle fluid
+                src={headshot}
+                alt="Alex Olsavsky Headshot"
+                />
+                </Box>
+  </Grid>
+</Grid>
+</Box>
+        
+        {/* <Box>
             <Container>
             <Box>
-                <img
+                <Image roundedCircle fluid
                 src={headshot}
                 alt="Alex Olsavsky Headshot"
                 />
             </Box>
             </Container>
-        </Box>
+        </Box> */}
         <About />
         </>
     )
--- src/components/About.js
+++ src/components/About.js
@@ -2,18 +2,17 @@ import React from 'react';
 import {
     Grid,
     Typography,
-    Box
+    Box,
+    Paper,
+    Icon
 } from '@mui/material';
+import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
+import { StayPrimaryLandscape } from '@mui/icons-material';
 
 function About() {
     return (
         <>
-        <Grid xs={12}>
-            <Typography variant="h2">
-                Hi, I'm Alex!
-            </Typography>
-        </Grid>
-        <Box>
+        <Paper elevation={3} sx={{ m: 5, p: 5}}>
             <Typography variant="h6">
                 I started my professional life as a professional opera singer, and now I'm a software developer! How did I make the leap? Read on.
             </Typography>
@@ -23,7 +22,10 @@ function About() {
             <Typography variant="h6">
             "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
             </Typography>
-        </Box>
+            <Typography variant="h5" sx={{ color: "primary.dark" }}>
+            View my work <ArrowCircleRightIcon />
+            </Typography>
+        </Paper>
         </>
         
     )
