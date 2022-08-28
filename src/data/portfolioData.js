5e3d57b0d3fe8a46934623611cd003584de3c22d
Alexandra Olsavsky <alexandraolsavsky@gmail.com>
2022-08-28 12:01:51 -0500

    Updated portfolio entries

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,6 +1,9 @@
 import TinyTriumphs from "../images/TinyTriumphs.png"
 import looapp from "../images/looapp.png"
 import weatherdashboard from "../images/weatherdashboard.png"
+import notetaker from "../images/notetaker.png"
+import employeetracker from "../images/employeetracker.png"
+import ecommerce from "../images/ecommerce.png"
 
 const portfolioData = [
     {
@@ -39,33 +42,34 @@ const portfolioData = [
     },
     {
         id: 4,
-        title: "Sample Title",
-        subheader: "More about the project",
-        image: "https://i.picsum.photos/id/1002/4312/2868.jpg?hmac=5LlLE-NY9oMnmIQp7ms6IfdvSUQOzP_O3DPMWmyNxwo",
-        description: "This is a project all about space! I am testing this now.",
-        deployedApp: "https://ajo-text-editor.herokuapp.com/",
-        githubRepo: "https://github.com/ajolsavsky",
-        technologies: "Node, MongoDB, special space junk"
+        title: "Note Taker",
+        subheader: "Web App with Express.js Backend",
+        image: notetaker,
+        description: "Write, save, and delete notes using an Express.js back end.",
+        deployedApp: "https://peaceful-bastion-70910.herokuapp.com/notes",
+        githubRepo: "https://github.com/ajolsavsky/note-taker",
+        technologies: "Node, Express.js, Heroku"
     },
     {
         id: 5,
-        title: "Another title",
-        subheader: "More about the project",
-        image: "https://i.picsum.photos/id/1003/1181/1772.jpg?hmac=oN9fHMXiqe9Zq2RM6XT-RVZkojgPnECWwyEF1RvvTZk",
-        description: "This is a project all about space! I am testing this now.",
-        deployedApp: "https://ajo-text-editor.herokuapp.com/",
-        githubRepo: "https://github.com/ajolsavsky",
-        technologies: "Node, MongoDB, special space junk"
+        title: "Employee Tracker",
+        subheader: "MySQL Command Line App",
+        image: employeetracker,
+        description: "This is a command-line application that allows you to manage a company's database of employees.",
+        deployedApp: "https://drive.google.com/file/d/1qHu8SXgKQXGtibfqxeXaAecW_aHC1rlD/view",
+        githubRepo: "https://github.com/ajolsavsky/employee-tracker",
+        technologies: "Node.js, Inquirer, MySQL"
     },
     {
         id: 6,
-        title: "A third project",
-        subheader: "More about the project",
-        image: "https://i.picsum.photos/id/1006/3000/2000.jpg?hmac=x83pQQ7LW1UTo8HxBcIWuRIVeN_uCg0cG6keXvNvM8g",
-        description: "This is a project all about space! I am testing this now.",
-        deployedApp: "https://ajo-text-editor.herokuapp.com/",
-        githubRepo: "https://github.com/ajolsavsky",
-        technologies: "Node, MongoDB, special space junk"
+        title: "E-Commerce Back-End",
+        subheader: "RESTful CRUD operations using Express.js/Sequelize",
+        image: ecommerce,
+        description: "This project creates the back end for an e-commerce app utilizing Express.js and Sequelize to interface with a MySQL database.",
+        deployedApp: "https://drive.google.com/file/d/1Oq6a6LyFhuD3mjjn4Co4ZfRlxlkI9Du3/view",
+        githubRepo: "https://github.com/ajolsavsky/e-commerce",
+        details: "Database created using MySQL through models and associations, and also demonstrates API routes to perform RESTful CRUD operations. Functionality of this app can be viewed in the walkthrough videos below.",
+        technologies: "Node, Express.js, Sequelize, MySQL, Insomnia"
     },
 ]
 
